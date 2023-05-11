6ecf4682ec500a781872bdb1b857f645c08f96ec
albertzhao <szdxzhy@outlook.com>
2023-05-11 11:11:03 +0800

    vuex store

---
+++ Vue.Basic/37 vuex store/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <Count />
+  </div>
+</template>
+
+<script>
+import Count from "./components/Count.vue";
+
+export default {
+  name: "App",
+  components: { Count },
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style>
+</style>
