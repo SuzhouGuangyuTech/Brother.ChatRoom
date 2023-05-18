aabd3441180e09cede51d0afe3dd768d1808a803
albertzhao <szdxzhy@outlook.com>
2023-05-18 14:27:03 +0800

    整理 vue2 源码

--- Vue.Basic/19 单文件组件/main.js
+++
@@ -1,7 +0,0 @@
-import App from './App.vue'
-
-new Vue({
-    el: "#app",
-    template: `<App></App>`,
-    components: { App }
-})
--- Vue.Basic/vue.config.js
+++ Vue.Basic/vue.config.js
@@ -1,27 +1,37 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  // 方式一：开启代理服务，转到后端服务器【优先匹配前端资源】
-  // devServer: {
-  //   proxy: 'http://localhost:5280'
-  // }
-
-  // 方式二：开启代理服务，转到后端服务器【加前缀方式】
-  devServer: {
-    proxy: {
-      '/albert1': { // 匹配所有以 '/albert1'开头的请求路径
-        target: 'http://localhost:5280', // 代理目标的基础路径
-        pathRewrite: { '^/albert1': '' }, // 代理往后端服务器的请求去掉 /albert1 前缀
-        // ws: true,												// WebSocket 默认为 false
-        // changeOrigin: true,							// 用于控制请求头中的 host 值，如果是 true 就关闭自己服务器端口来源，和最终请求的服务器保持一直（说谎），改为 false 则如实回答，不说谎。
-
-      },
-      '/demo': {
-        target: 'http://localhost:5281',
-        pathRewrite: { '^/demo': '' },
-        changeOrigin: true
-      }
+//common js 暴露
+module.exports = {
+    pages: {
+        index: {
+            //入口
+            entry: 'src/main.js',
+        },
+    },
+    //关掉默认的语法检查
+    lintOnSave: false,
+    // //开启代理服务器(方式一)
+    // devServer:{
+    //     proxy: 'http://localhost:5001', //将请求转发给端口号5001，注意这里只能配置单个代理，不能配置多个代理
+    // }
+    //开启代理服务器(方式二) 多个代理
+    devServer: {
+        proxy: {
+            //当请求的前缀是api，直接转发请求到服务器5001端口
+            '/api': {
+                target: 'http://localhost:5001',
+                ws: true, //用于支持websocket
+                changeOrigin: true, //用于控制请求头中host的值
+                pathRewrite:{
+                    //把请求中含有的api替换成空字符串
+                    '^/api': '',
+                }
+            },
+            '/demo': {
+                target: 'http://localhost:5002',
+                //不写 ws和changeOrigin默认为true
+                pathRewrite: {
+                    '^/demo':'',
+                }
+            }
+        }
     }
-  }
-
-})
+}
